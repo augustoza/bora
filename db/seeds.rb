e6d43cf6566a3fb7b15928cbfbbde59ab1231845
diego fernandes <diegoc.fernandes1@gmail.com>
2020-11-30 18:21:59 -0300

    user seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#TODO USERS
+
+user1 = User.create(name: 'Diego C', username: 'Diego', country: 'Brazil', genre: 'male', email: 'diego@teste.com', )
+user2 = User.create(name: 'Augusto AZ', username: 'Augustoza', country: 'Germany', genre: 'male', email: 'augusto@teste.com', )
+user3 = User.create(name: 'Beatriz Caram', username: 'Bia', country: 'Mexico', genre: 'female', email: 'biacaram@teste.com', )
+user4 = User.create(name: 'Thiago', username: 'Thiagoav', country: 'Uruguay', genre: 'male', email: 'thiagoav@teste.com', )
+user5 = User.create(name: 'Roberto', username: 'Bebeto', country: 'Japan', genre: 'male', email: 'bebeto@teste.com', )
+user6 = User.create(name: 'Laranjeira', username: 'Laranjeira', country: 'China', genre: 'female', email: 'laranja@teste.com', )
+
+activity1 = Activity.create(title: '', initial_date: DateTime.now, final_date: DateTime.now, location: '')
+activity2 = Activity.create(title: '', initial_date: DateTime.now, final_date: DateTime.now, location: '')
+activity3 = Activity.create(title: '', initial_date: DateTime.now, final_date: DateTime.now, location: '')
+activity4 = Activity.create(title: '', initial_date: DateTime.now, final_date: DateTime.now, location: '')
+
