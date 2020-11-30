a68d0f332820d385f94556c9750198ca5e4bca10
augusto araujo <augusto.za@gmail.com>
2020-11-30 15:34:44 -0300

    changed coordinates to latitude - longitude. remade review table

--- db/migrate/20201130183140_create_reviews.rb
+++ db/migrate/20201130183140_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :exploration, null: false, foreign_key: true
       t.integer :rating
       t.text :content
-      t.references :user, null: false, foreign_key: true
-      t.references :reviewed_user, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
