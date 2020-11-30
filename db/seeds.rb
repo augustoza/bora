040fb7b3da3bbb9ff1c72deda4373af521507c1d
diego fernandes <diegoc.fernandes1@gmail.com>
2020-11-30 18:51:59 -0300

    seeds ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,24 @@ require 'date'
 
 #TODO USERS
 
-# User.create!(password: '123abc', full_name: 'Diego C', username: 'Diego', country: 'Brazil', genre: 'male', email: 'diego@teste.com')
+User.create!(password: '123abc', full_name: 'Diego C', username: 'Diego', country: 'Brazil', genre: 'male', email: 'diego@teste.com')
 
-# User.create!(password: '123abc', full_name: 'Augusto AZ', username: 'Augustoza', country: 'Germany', genre: 'male', email: 'augusto@teste.com')
+User.create!(password: '123abc', full_name: 'Augusto AZ', username: 'Augustoza', country: 'Germany', genre: 'male', email: 'augusto@teste.com')
 
-# User.create!(password: '123abc', full_name: 'Beatriz Caram', username: 'Bia', country: 'Mexico', genre: 'female', email: 'biacaram@teste.com')
+User.create!(password: '123abc', full_name: 'Beatriz Caram', username: 'Bia', country: 'Mexico', genre: 'female', email: 'biacaram@teste.com')
 
-# User.create!(password: '123abc', full_name: 'Thiago', username: 'Thiagoav', country: 'Uruguay', genre: 'male', email: 'thiagoav@teste.com')
+User.create!(password: '123abc', full_name: 'Thiago', username: 'Thiagoav', country: 'Uruguay', genre: 'male', email: 'thiagoav@teste.com')
 
-# User.create!(password: '123abc', full_name: 'Roberto', username: 'Bebeto', country: 'Japan', genre: 'male', email: 'bebeto@teste.com')
+User.create!(password: '123abc', full_name: 'Roberto', username: 'Bebeto', country: 'Japan', genre: 'male', email: 'bebeto@teste.com')
 
-# User.create!(password: '123abc', full_name: 'Laranjeira', username: 'Laranjeira', country: 'China', genre: 'female', email: 'laranja@teste.com')
+User.create!(password: '123abc', full_name: 'Laranjeira', username: 'Laranjeira', country: 'China', genre: 'female', email: 'laranja@teste.com')
 
-# finaldate = '<2021-11-30T18:32:38-03:00>'
+finaldate = (DateTime.now +99)
 
-# Activity.create!(title: 'Explorar Buenos Aires', initial_date: DateTime.now, final_date: finaldate, location: 'Buenos Aires')
+Activity.create!(user_id: 1, title: 'Explorar Buenos Aires', initial_date: DateTime.now, final_date: finaldate, location: 'Buenos Aires')
 
-# Activity.create!(title: 'Explorar o Louvre', initial_date: DateTime.now, final_date: finaldate, location: 'Paris')
+Activity.create!(user_id: 2, title: 'Explorar o Louvre', initial_date: DateTime.now, final_date: finaldate, location: 'Paris')
 
-# Activity.create!(title: 'Grab a drink', initial_date: DateTime.now, final_date: finaldate, location: 'London')
+Activity.create!(user_id: 3, title: 'Grab a drink', initial_date: DateTime.now, final_date: finaldate, location: 'London')
 
-# Activity.create!(title: 'Explore the area', initial_date: DateTime.now, final_date: finaldate, location: 'Tokyo')
+Activity.create!(user_id: 4, title: 'Explore the area', initial_date: DateTime.now, final_date: finaldate, location: 'Tokyo')
