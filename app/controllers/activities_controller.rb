baa311d745292ef578684c926a68866688109334
diego fernandes <diegoc.fernandes1@gmail.com>
2020-12-01 11:32:06 -0300

    controller done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,11 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update]
+  
   def index
     @activities = Activity.all
   end
 
-  def show
-    @activity = Activity.find(params[:id])
-  end
+  def show; end
 
   def new
     @activity = Activity.new
@@ -15,19 +15,28 @@ class ActivitiesController < ApplicationController
     @activity = Activity.new(activity_params)
     @activity.user = current_user
 
-    if @product
+    if @activity.save
+      redirect_to activity_path(@activity), notice: "Activity created"
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    @activity.update(activity_params)
+    redirect_to activity_path(@activity), notice: "Activity updated"
   end
 
 private
 
 def activity_params
   params.require(:activity).permit(:title, :location, :description, :initial_date, :final_date)
+end
+
+def set_activity
+  @activity = Activity.find(params[:id])
 end
 
 end
