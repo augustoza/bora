505ea1443d8ca114c12805e722e3cc605951caf7
diego fernandes <diegoc.fernandes1@gmail.com>
2020-12-01 11:25:43 -0300

    index, show, new

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,14 +1,21 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.all
   end
 
   def show
+    @activity = Activity.find(params[:id])
   end
 
   def new
+    @activity = Activity.new
   end
 
   def create
+    @activity = Activity.new(activity_params)
+    @activity.user = current_user
+
+    if @product
   end
 
   def edit
@@ -16,4 +23,11 @@ class ActivitiesController < ApplicationController
 
   def update
   end
+
+private
+
+def activity_params
+  params.require(:activity).permit(:title, :location, :description, :initial_date, :final_date)
+end
+
 end
