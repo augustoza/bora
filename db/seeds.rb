71d03c0a6f522c9d4045a2dfcf4f363a96d63af3
diego fernandes <diegoc.fernandes1@gmail.com>
2020-12-11 13:35:41 -0300

    users ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,57 @@ require 'open-uri'
 #TODO -----------> USERS <---------------
 #TODO------------------------------------
 
-User.create!(password: '123abc', full_name: 'Diego C', username: 'Diego', country: 'Brazil', genre: 'male', email: 'diego@teste.com')
+diego = User.create!(password: '123abc', full_name: 'Diego Correa', username: 'Diego', country: 'Brazil', genre: 'male', email: 'diego@teste.com')
+file = URI.open('https://avatars2.githubusercontent.com/u/71459501?v=4')
+diego.photo.attach(io: file, filename: 'diego.jpg', content_type: 'image/jpg')
 
-User.create!(password: '123abc', full_name: 'Augusto AZ', username: 'Augustoza', country: 'Germany', genre: 'male', email: 'augusto@teste.com')
+augusto = User.create!(password: '123abc', full_name: 'Augusto AZ', username: 'Augustoza', country: 'Uruguay', genre: 'male', email: 'augusto@teste.com')
+file = URI.open('https://avatars0.githubusercontent.com/u/72287676?v=4')
+augusto.photo.attach(io: file, filename: 'augusto.jpg', content_type: 'image/jpg')
 
-User.create!(password: '123abc', full_name: 'Beatriz Caram', username: 'Bia', country: 'Mexico', genre: 'female', email: 'biacaram@teste.com')
+beatriz = User.create!(password: '123abc', full_name: 'Beatriz Caram', username: 'Bia', country: 'Mexico', genre: 'female', email: 'biacaram@teste.com')
+file = URI.open('https://avatars3.githubusercontent.com/u/60752820?v=4')
+beatriz.photo.attach(io: file, filename: 'beatriz.jpg', content_type: 'image/jpg')
 
-User.create!(password: '123abc', full_name: 'Thiago', username: 'Thiagoav', country: 'Uruguay', genre: 'male', email: 'thiagoav@teste.com')
+mark = User.create!(password: '123abc', full_name: 'Mark Li', username: 'Mark', country: 'Germany', genre: 'male', email: 'mark@teste.com')
+file = URI.open('https://avatars3.githubusercontent.com/u/71938485?v=4')
+mark.photo.attach(io: file, filename: 'mark.jpg', content_type: 'image/jpg')
 
-User.create!(password: '123abc', full_name: 'Roberto', username: 'Bebeto', country: 'Japan', genre: 'male', email: 'bebeto@teste.com')
+bebeto = User.create!(password: '123abc', full_name: 'Roberto Chulapa', username: 'Bebeto', country: 'Japan', genre: 'male', email: 'bebeto@teste.com')
+file = URI.open('https://s1.imghub.io/abbBy.jpg')
+bebeto.photo.attach(io: file, filename: 'bebeto.jpg', content_type: 'image/jpg')
 
-laranjeira = User.create!(password: '123abc', full_name: 'Laranjeira', username: 'Laranjeira', country: 'China', genre: 'female', email: 'laranja@teste.com')
+laranjeira = User.create!(password: '123abc', full_name: 'Laranjeira Bonita', username: 'Laranjeira', country: 'China', genre: 'female', email: 'laranja@teste.com')
 file = URI.open('https://avatars1.githubusercontent.com/u/69615451?v=4')
 laranjeira.photo.attach(io: file, filename: 'laranjeira.jpg', content_type: 'image/jpg')
 
-User.create!(password: '123123', full_name: 'Joleno', username: 'joleno', country: 'Brazil', genre: 'male', email: 'joleno@teste.com')
+lincoln = User.create!(password: '123abc', full_name: 'Lincoln Abraham', username: 'Lincoln', about: 'Sou baixista da banda Lifetime Code, adoro codar, sentir o vento batendo em meu corpo nu. Estou aqui para conhecer diversas pessoas e me divertir muito. Manda nudes!' country: 'Brazil', genre: 'male', email: 'lincoln@teste.com')
+file = URI.open('https://avatars2.githubusercontent.com/u/66792417?v=4')
+lincoln.photo.attach(io: file, filename: 'lincoln.jpg', content_type: 'image/jpg')
+
+miguel = User.create!(password: '123abc', full_name: 'Miguel Ito', username: 'Miguel', country: 'Portugal', genre: 'male', email: 'miguel@teste.com')
+file = URI.open('https://avatars2.githubusercontent.com/u/64758573?v=4')
+miguel.photo.attach(io: file, filename: 'miguel.jpg', content_type: 'image/jpg')
+
+renata = User.create!(password: '123abc', full_name: 'Renata Ingrata', username: 'Renata', country: 'Spain', genre: 'female', email: 'renata@teste.com')
+file = URI.open('https://avatars1.githubusercontent.com/u/70242792?v=4')
+renata.photo.attach(io: file, filename: 'renata.jpg', content_type: 'image/jpg')
+
+vitor = User.create!(password: '123abc', full_name: 'Vitor Dal Pra', username: 'Vitor', country: 'England', genre: 'male', email: 'vitor@teste.com')
+file = URI.open('https://avatars3.githubusercontent.com/u/21279236?v=4')
+vitor.photo.attach(io: file, filename: 'vitor.jpg', content_type: 'image/jpg')
+
+carla = User.create!(password: '123abc', full_name: 'Carla Perez', username: 'Carla', country: 'Argentina', genre: 'female', email: 'carla@teste.com')
+file = URI.open('https://avatars2.githubusercontent.com/u/72417551?v=4')
+carla.photo.attach(io: file, filename: 'carla.jpg', content_type: 'image/jpg')
+
+afonso = User.create!(password: '123abc', full_name: 'Afonso Augusto', username: 'Afonso', country: 'Jamaica', genre: 'male', email: 'afonso@teste.com')
+file = URI.open('https://avatars2.githubusercontent.com/u/66492757?v=4')
+afonso.photo.attach(io: file, filename: 'afonso.jpg', content_type: 'image/jpg')
+
+fernando = User.create!(password: '123abc', full_name: 'Fernando Beira-mar', username: 'Fernando', country: 'Australia', genre: 'male', email: 'fernando@teste.com')
+file = URI.open('https://avatars3.githubusercontent.com/u/71726856?v=4')
+fernando.photo.attach(io: file, filename: 'fernando.jpg', content_type: 'image/jpg')
 
 #TODO------------------------------------
 #TODO---------> ACTIVITIES <-------------
@@ -28,11 +64,11 @@ User.create!(password: '123123', full_name: 'Joleno', username: 'joleno', countr
 finaldate = (DateTime.now +99)
 CATEGORIES = ['Ao ar livre', 'Natureza', 'Aventura', 'Cultural', 'Festas', 'Bares', 'Shows', 'Gastronomia', 'Compras', 'Transporte', 'Esportes']
 
-activity_1 = Activity.create!(category: CATEGORIES.sample, user_id: 1, title: 'Explorar Buenos Aires', initial_date: DateTime.now, final_date: finaldate, location: 'Buenos Aires')
+activity_1 = Activity.create!(category: 'Cultural', user_id: 1, title: 'Conhecer o MASP', about: 'Bora conhecer esse museu, curtir uma manhã e tarde inteira lá!', initial_date: DateTime.now, final_date: finaldate, location: 'MASP, São Paulo')
 Chatroom.create!(activity_id: activity_1.id)
 Exploration.create!(activity_id: activity_1.id, user_id: 1)
 
-activity_2 = Activity.create!(category: CATEGORIES.sample, user_id: 2, title: 'Explorar o Louvre', initial_date: DateTime.now, final_date: finaldate, location: 'Paris')
+activity_2 = Activity.create!(category: 'Festas', user_id: 2, title: 'Party hard', about: 'Vamos curtir todas as festas possíveis em uma noite só, tipo pubcrawl!', initial_date: DateTime.now, final_date: finaldate, location: 'Paris')
 Chatroom.create!(activity_id: activity_2.id)
 Exploration.create!(activity_id: activity_2.id, user_id: 2)
 
